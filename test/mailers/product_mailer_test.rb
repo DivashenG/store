988afddca8ae67e0d4139e389bef2fa26bca00a1
Divashen <39258459+DivashenG@users.noreply.github.com>
2025-01-02 13:34:13 +0200

    Add better fixtures. (#16)

--- test/mailers/product_mailer_test.rb
+++ test/mailers/product_mailer_test.rb
@@ -2,10 +2,11 @@ require "test_helper"
 
 class ProductMailerTest < ActionMailer::TestCase
   test "in_stock" do
-    mail = ProductMailer.with(product: self, subscriber: subscribers(:one)).in_stock
+    mail = ProductMailer.with(product: self, subscriber: subscribers(:david)).in_stock
 
     assert_equal "In stock", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
+    assert_equal [ "david@example.org" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
+    assert_match "Good news!", mail.body.encoded
   end
 end
